fe25a67a33920e8dea49dcb95ee017ad5447e5f2
Zaid Huda <hudadiaz@gmail.com>
2021-10-30 13:22:15 +0800

    Show loader and current day indicator in attendance

--- src/components/Attendance/Day.tsx
+++ src/components/Attendance/Day.tsx
@@ -4,7 +4,12 @@ import useAttendances from 'hooks/useAttendances';
 import { Profile } from 'types/profile';
 import { DateTime } from 'luxon';
 import useFirebase from 'hooks/useFirebase';
-import { Avatar, AvatarGroup, ButtonBase } from '@mui/material';
+import {
+  Avatar,
+  AvatarGroup,
+  ButtonBase,
+  CircularProgress,
+} from '@mui/material';
 
 interface Props {
   date: DateTime;
@@ -12,6 +17,7 @@ interface Props {
 
 export default function AttendanceDay({ date }: Props) {
   const avatarsContainer = useRef<HTMLDivElement | null>(null);
+  const [loading, setLoading] = useState(false);
   const [submitting, setSubmitting] = useState(false);
   const { user, getConfig } = useFirebase();
   const { getAttendances, addAttendance, removeAttendance } = useAttendances();
@@ -36,8 +42,10 @@ export default function AttendanceDay({ date }: Props) {
   };
 
   useEffect(() => {
+    setLoading(true);
     return getAttendances(date, (results) => {
       setAttendances(results);
+      setLoading(false);
     });
   }, [getAttendances, date]);
 
@@ -47,17 +55,27 @@ export default function AttendanceDay({ date }: Props) {
 
   const attendeesCount = attendances.length;
 
+  const today = date.toISODate() === DateTime.now().toISODate();
   const past = date < DateTime.now().startOf('day');
   const full = attendeesCount >= MAX_ATTENDEES && !hasUser;
   const disabled = past || full;
 
-  let style = { border: 'border-gray-300', text: 'text-gray-400' };
+  let style = {
+    border: 'border-gray-300 ring-gray-100',
+    text: 'text-gray-400',
+  };
   if (attendeesCount >= 1)
-    style = { border: 'border-green-500', text: 'text-green-500' };
+    style = {
+      border: 'border-green-500 ring-green-100',
+      text: 'text-green-500',
+    };
   if (attendeesCount >= MAX_ATTENDEES / 2)
-    style = { border: 'border-yellow-500', text: 'text-yellow-500' };
+    style = {
+      border: 'border-yellow-500 ring-yellow-100',
+      text: 'text-yellow-500',
+    };
   if (attendeesCount >= MAX_ATTENDEES)
-    style = { border: 'border-red-500', text: 'text-red-500' };
+    style = { border: 'border-red-500 ring-red-100', text: 'text-red-500' };
 
   const CountContent = () => (
     <>
@@ -79,7 +97,7 @@ export default function AttendanceDay({ date }: Props) {
         className={classnames(
           'border-2 flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4 h-[96px] sm:h-[92px] p-2 sm:p-4 rounded w-full',
           style.border,
-          { 'text-gray-400': past }
+          { 'text-gray-400': past, 'ring-4': today }
         )}
       >
         <div className="flex sm:flex-col items-baseline sm:items-start justify-between min-w-[120px]">
@@ -101,16 +119,20 @@ export default function AttendanceDay({ date }: Props) {
           </>
         </div>
         <div ref={avatarsContainer} className="flex flex-1">
-          <AvatarGroup max={maxAvatars}>
-            {attendances.map(({ photoURL, displayName }) => (
-              <Avatar
-                key={photoURL}
-                src={photoURL}
-                alt={displayName}
-                title={displayName}
-              />
-            ))}
-          </AvatarGroup>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <AvatarGroup max={maxAvatars}>
+              {attendances.map(({ photoURL, displayName }) => (
+                <Avatar
+                  key={photoURL}
+                  src={photoURL}
+                  alt={displayName}
+                  title={displayName}
+                />
+              ))}
+            </AvatarGroup>
+          )}
         </div>
         <div
           className={classnames(
